81cc05c176ab941baef924afefeed0ab40dd7ee0
iron-cherep <jacobxblue@gmail.com>
2017-02-12 17:38:00 +0300

    добавил некоторые мелкие элементы, сделал тестовую анимацию и плавный скроллинг указателей

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+window.onload = function() {
+              
+  var toServices = document.getElementById("go-to-services");
+  toServices.addEventListener("click", function() {
+    document.getElementById("services").scrollIntoView({ behavior: 'smooth' });
+  });
+  
+  var toProjects = document.getElementById("go-to-projects");
+  toProjects.addEventListener("click", function() {
+    document.getElementById("projects").scrollIntoView({ behavior: 'smooth' });
+  });
+  
+}
