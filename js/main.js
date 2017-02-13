8c5da20668bbc7f34216aa28a5c64d69722eb9a8
iron-cherep <jacobxblue@gmail.com>
2017-02-13 08:16:23 +0300

    закончил адаптивные стили и анимации

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 window.onload = function() {
-              
+          
+  addSmoothScrollButtons();              
+  addNavbarButton();
+  
+}
+
+
+function addSmoothScrollButtons() {
   var toServices = document.getElementById("go-to-services");
   toServices.addEventListener("click", function() {
     document.getElementById("services").scrollIntoView({ behavior: 'smooth' });
@@ -9,5 +16,29 @@ window.onload = function() {
   toProjects.addEventListener("click", function() {
     document.getElementById("projects").scrollIntoView({ behavior: 'smooth' });
   });
+}
+
+function addNavbarButton() {
   
+  var navbar = document.getElementById("navbar");
+  var navbarButton = document.getElementById("navbar-button");
+  var navbarIcon = document.getElementById("navbar-icon");
+  closeNavbar();
+  
+  navbarButton.addEventListener("click", function() {
+    if (navbarOpened) { closeNavbar(); }
+    else { openNavbar(); }          
+  }); 
+  
+  function closeNavbar() {
+    navbar.classList.add("navigation__menu--collapsed");
+    navbarIcon.classList.remove("navigation__button-bar--collapsed");
+    
+    navbarOpened = false;
+  }
+  function openNavbar() {
+    navbar.classList.remove("navigation__menu--collapsed");
+    navbarIcon.classList.add("navigation__button-bar--collapsed");
+    navbarOpened = true;
+  }    
 }
